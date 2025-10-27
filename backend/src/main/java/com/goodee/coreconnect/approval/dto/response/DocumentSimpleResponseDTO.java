e82d8237017e049a347712ffe88ca43e65907220
109kms <109kms@naver.com>
2025-10-27 12:11:18 +0900

    RequestDTO, ResponseDTO 추가

---
+++ backend/src/main/java/com/goodee/coreconnect/approval/dto/response/DocumentSimpleResponseDTO.java
@@ -0,0 +1,34 @@
+package com.goodee.coreconnect.approval.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.goodee.coreconnect.approval.entity.Document;
+import com.goodee.coreconnect.approval.enums.DocumentStatus;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * 문서 목록 조회 시 사용될 DTO ("내가 상신한 문서", "결재할 문서" 등 목록을 조회할 때 사용하는 가벼운 DTO)
+ */
+@Getter
+@Builder
+public class DocumentSimpleResponseDTO {
+
+  private Integer documentId;
+  private String documentTitle;
+  private String writerName;
+  private DocumentStatus documentStatus;
+  private LocalDateTime createdAt;
+  
+  public static DocumentSimpleResponseDTO toDTO(Document document) {
+    return DocumentSimpleResponseDTO.builder()
+        .documentId(document.getId())
+        .documentTitle(document.getDocumentTitle())
+        .writerName(document.getUser().getName())
+        .documentStatus(document.getDocumentStatus())
+        .createdAt(document.getCreatedAt())
+        .build();
+  }
+  
+}
