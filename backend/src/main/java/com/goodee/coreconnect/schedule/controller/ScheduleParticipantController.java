84c8344a17de2853fe66cf99b62af9073985d5cd
leedong <ehddnrzxc@naver.com>
2025-10-29 17:56:34 +0900

    Schedule(일정) Controller들의 swagger 어노테이션 추가

--- backend/src/main/java/com/goodee/coreconnect/schedule/controller/ScheduleParticipantController.java
+++ backend/src/main/java/com/goodee/coreconnect/schedule/controller/ScheduleParticipantController.java
@@ -16,17 +16,23 @@ import com.goodee.coreconnect.schedule.dto.request.RequestScheduleParticipantDTO
 import com.goodee.coreconnect.schedule.dto.response.ResponseScheduleParticipantDTO;
 import com.goodee.coreconnect.schedule.service.ScheduleParticipantService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "ScheduleParticpant API", description = "일정참여자 관리 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/scheduleParticipants")
+@SecurityRequirement(name = "bearerAuth")
 public class ScheduleParticipantController {
 
   private final ScheduleParticipantService participantService;
 
   /** 참여자 추가 */
+  @Operation(summary = "참여자 추가", description = "참여자를 추가합니다.")
   @PostMapping
   public ResponseScheduleParticipantDTO add(@Valid @RequestBody RequestScheduleParticipantDTO dto, @AuthenticationPrincipal String email) {
     
@@ -34,18 +40,21 @@ public class ScheduleParticipantController {
   }
 
   /** 일정별 참여자 목록 조회 */
+  @Operation(summary = "참여자 목록 조회", description = "참여자 목록을 조회합니다.")
   @GetMapping(params = "scheduleId")
   public List<ResponseScheduleParticipantDTO> getBySchedule(@RequestParam("scheduleId") Integer scheduleId) {
     return participantService.getParticipantsBySchedule(scheduleId);
   }
 
   /** 유저별 참여 일정 목록 조회 */
+  @Operation(summary = "참여자 일정 조회", description = "참여자의 일정을 조회합니다.")
   @GetMapping(params = "userId")
   public List<ResponseScheduleParticipantDTO> getByUser(@RequestParam("userId") Integer userId) {
     return participantService.getSchedulesByUser(userId);
   }
 
   /** 참여자 삭제 (Soft Delete) */
+  @Operation(summary = "참여자", description = "일정에서 참여자를 삭제합니다.")
   @DeleteMapping("/{id}")
   public void delete(@PathVariable("id") Integer id) {
     participantService.deleteParticipant(id);
