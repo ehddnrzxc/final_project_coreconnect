5262b185347ef72b2bee4404c186a36f3819d42c
Eusky <yoochun8128@gmail.com>
2025-10-31 17:10:00 +0900

    관리자 콘솔 페이지 추가

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import App from "./App";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import UserCreateForm from "./components/admin/UserCreateForm";
 import AdminRoute from "./components/admin/AdminRoute";
 import useAuth from "./hooks/useAuth";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import ApprovalHomePage from './pages/ApprovalHomePage';
-import ApprovalWritePage from './pages/ApprovalWritePage';
-import ApprovalLayout from './pages/ApprovalLayout';
+import ApprovalHomePage from "./pages/ApprovalHomePage";
+import ApprovalWritePage from "./pages/ApprovalWritePage";
+import ApprovalLayout from "./pages/ApprovalLayout";
+import AdminHome from "./components/admin/AdminHome";
+import UserListPage from "./components/admin/UserListPage";
 
 /* 로그인 보호용 라우트 */
 function ProtectedRoute({ children }) {
@@ -33,42 +40,43 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: "e-approval",
-        element: (
-          <ProtectedRoute>
-            <ApprovalLayout /> {/* 2단 레이아웃 껍데기 */}
-          </ProtectedRoute>
-        ),
-        children: [
-          {
-            index: true, // /e-approval 접속 시 기본으로 보일 페이지
-            element: <ApprovalHomePage />,
-          },
-          {
-            path: "new", // /e-approval/new (새 결재 진행)
-            element: <ApprovalWritePage />,
-          },
-          {
-            path: "forms", // /e-approval/forms (자주 쓰는 양식)
-            element: <div>자주 쓰는 양식 페이지</div>, // 임시
-          },
-          {
-            path: "pending", // /e-approval/pending (결재 대기 문서)
-            element: <div>결재 대기 문서 목록</div>, // 임시
-          },
-          {
-            path: "doc/:documentId", // /e-approval/doc/123 (결재 상세)
-            element: <div>결재 상세 페이지</div>, // 임시
-          }
-        ]
-      },
-      {
-        path: "admin/users/create",
+        path: "e-approval",
         element: (
-          <AdminRoute>
-            <UserCreateForm />
-          </AdminRoute>
+          <ProtectedRoute>
+            <ApprovalLayout /> {/* 2단 레이아웃 껍데기 */}
+          </ProtectedRoute>
         ),
+        children: [
+          {
+            index: true, // /e-approval 접속 시 기본으로 보일 페이지
+            element: <ApprovalHomePage />,
+          },
+          {
+            path: "new", // /e-approval/new (새 결재 진행)
+            element: <ApprovalWritePage />,
+          },
+          {
+            path: "forms", // /e-approval/forms (자주 쓰는 양식)
+            element: <div>자주 쓰는 양식 페이지</div>, // 임시
+          },
+          {
+            path: "pending", // /e-approval/pending (결재 대기 문서)
+            element: <div>결재 대기 문서 목록</div>, // 임시
+          },
+          {
+            path: "doc/:documentId", // /e-approval/doc/123 (결재 상세)
+            element: <div>결재 상세 페이지</div>, // 임시
+          },
+        ],
+      },
+      {
+        path: "admin",
+        element: <AdminRoute />,
+        children: [
+          { path: "", element: <AdminHome /> },
+          { path: "users/create", element: <UserCreateForm /> },
+          { path: "users", element: <UserListPage /> },
+        ],
       },
       {
         index: true,
